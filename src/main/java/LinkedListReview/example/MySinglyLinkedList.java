8d702ae0553b632e0164f1a06b59272845da22e8
canantu <tunc.canan84@gmail.com>
2024-09-05 22:39:01 +0200

    [+] SinglyListedList

---
+++ src/main/java/LinkedListReview/example/MySinglyLinkedList.java
@@ -0,0 +1,64 @@
+package LinkedListReview.example;
+
+import java.util.ArrayList;
+
+public class MySinglyLinkedList {
+
+    Node head;
+    Node tail;
+    int size;
+
+    boolean isEmpty(){
+        return head == null;
+    }
+
+
+    void add(int data){
+        Node newNode = new Node(data);
+        if(isEmpty()){
+            head = tail = newNode;
+        } else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    void deleteById(int id){
+        // check if empty
+        if (isEmpty()){
+            System.out.println("List is empty");
+        }
+        Node prev = head;
+        Node current = head;
+
+        while(current != null){
+            if (current.id == id){
+                if (current == head){
+                    head = current.next;
+                    current.next = null;
+                }else if (current == tail){
+                    tail = prev;
+                    prev.next = null;
+                }else{
+                    prev.next = current.next;
+                    current.next = null;
+                }
+                size--;
+            }
+            // move to the other elements
+            prev = current;
+            current = current.next;
+        }
+    }
+
+    void printNodes(){
+        Node current = head;
+        while (current != null){
+            if(current.next != null){
+                System.out.print(current.id + " => ");
+            }else System.out.print(current.id + " => null");
+            current = current.next;
+        }
+    }
+}
---
+++ src/main/java/LinkedListReview/example/SinglyListedListApp.java
@@ -0,0 +1,20 @@
+package LinkedListReview.example;
+
+public class SinglyListedListApp {
+
+    public static void main(String[] args) {
+
+        MySinglyLinkedList myList = new MySinglyLinkedList();
+        System.out.println(myList.isEmpty());
+
+        for (int i = 0; i < 10; i++) {
+            myList.add(i);
+        }
+
+        myList.printNodes();
+
+        myList.deleteById(5);
+        System.out.println();
+        myList.printNodes();
+    }
+}
