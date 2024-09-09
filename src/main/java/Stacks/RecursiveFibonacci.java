7c80b5241eea520049259402ece6652696513a28
canantu <tunc.canan84@gmail.com>
2024-09-09 21:30:23 +0200

    [+] recursion

---
+++ src/main/java/Stacks/RecursiveFibonacci.java
@@ -0,0 +1,16 @@
+package Stacks;
+
+public class RecursiveFibonacci {
+
+    public static void main(String[] args) {
+
+        System.out.println(fib(4));
+    }
+
+
+    public static int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+}
